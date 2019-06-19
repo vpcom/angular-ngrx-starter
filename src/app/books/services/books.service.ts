4cf14d6fd09e033a4b6119047dd8a0d093eebb38
Vincent Perrin <vpcom01@gmail.com>
2019-06-19 12:41:52 +0200

    feature - created a debug mode

--- src/app/books/services/books.service.ts
+++ src/app/books/services/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Book } from '../books.model';
 import { Observable, of } from 'rxjs';
 import { LocalStorageService } from 'src/app/local-storage/local-storage.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,14 @@ export class BooksService {
 
   constructor(private localStorageService: LocalStorageService) { }
 
+  /**
+   * Loads JSON data representing a list of all books including minimal data.
+   */
   public getAllBooks(): Observable<Book[]> {
-    console.log('Fake HTTP request getAllBooks called');
+    if (environment.trace) {
+      console.log('HTTP request: getAllBooks');
+    }
+    
     return of(this.localStorageService.getItem('books'));
   }
 }
