4393b7ed4e6da81797df81e7af43f21e0bc12b8e
Vincent Perrin <vpcom01@gmail.com>
2019-06-20 11:43:40 +0200

    style(material): customization for mat-card display in list

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { MaterialModule } from '../app.angular.material.module';
+
 import { BooksRoutingModule } from './books-routing.module';
 import { BooksContainerComponent } from './books-container/books-container.component';
-import { StoreModule } from '@ngrx/store';
 import * as fromBooks from './store/books.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { BooksEffects } from './store/books.effects';
 
 @NgModule({
@@ -13,6 +16,9 @@ import { BooksEffects } from './store/books.effects';
   imports: [
     CommonModule,
     BooksRoutingModule,
+
+    MaterialModule,
+    
     StoreModule.forFeature('books', fromBooks.reducer),
     EffectsModule.forFeature([BooksEffects]),
   ]
