3c5c5aa8bb1b294a00ecde94845e7935eacb060e
Vincent Perrin <vpcom01@gmail.com>
2019-10-02 16:27:50 +0200

    test(books): default service case

--- src/app/services/books.service.spec.ts
+++ src/app/services/books.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BooksService } from './books.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BooksService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [BooksService]
+  }));
 
   it('should be created', () => {
     const service: BooksService = TestBed.get(BooksService);
