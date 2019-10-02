81d4bdde660626f81757ea3a73242462fa73445f
Vincent Perrin <vpcom01@gmail.com>
2019-10-02 14:50:49 +0200

    test(localStorage): default test case

--- src/app/local-storage/local-storage.service.spec.ts
+++ src/app/local-storage/local-storage.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LocalStorageService } from './local-storage.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LocalStorageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [LocalStorageService]
+  }));
 
   it('should be created', () => {
     const service: LocalStorageService = TestBed.get(LocalStorageService);
